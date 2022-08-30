69fa5b8db279d39b25f28d61ac06513a1db2a569
BiancaAdS <42790720+BiancaAdS@users.noreply.github.com>
2022-08-29 21:42:33 -0300

    inclusão de novos campo equipe

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Equipes
 
 class EquipesAdmin(admin.ModelAdmin):
-  list = ('nomeDaEquipe', 'equipeAtual', 'quantidadeIntegrantes', 'quantidadeIntegrantes', 'seConhecem', 'etapaFinalizada', 'definidor', 'facilitador', 'responsavelTempo' ,'linkRetrospectiva1','linkRetrospectiva2','linkRetrospectiva3', 'linkRetrospectiva4')
+  list = ('nomeDaEquipe', 'equipeAtual', 'quantidadeIntegrantes', 'quantidadeIntegrantes', 'seConhecem', 'etapaFinalizada', 'definidor', 'facilitador', 'observador' , 'entrevistador' , 'scrumMaster' ,'linkRetrospectiva1','linkRetrospectiva2','linkRetrospectiva3', 'linkRetrospectiva4')
 
 admin.site.register(Equipes, EquipesAdmin)
