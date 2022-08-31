4926a6b688295a19fd10e709a666be8c7a642543
BiancaAdS <42790720+BiancaAdS@users.noreply.github.com>
2022-08-30 22:08:04 -0300

    primeira versão ajustes 14

--- fast_design_sprint/settings.py
+++ fast_design_sprint/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '9nxl-95wsdi6i+1drrksz=h!8@#o3rjj&&j_v1mab*a&!%g19i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
