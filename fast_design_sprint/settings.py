14196ee17f1b4ffedc4d607c60d7bbdf743333af
BiancaAdS <42790720+BiancaAdS@users.noreply.github.com>
2022-08-30 22:50:48 -0300

    primeira versão ajustes 18

--- fast_design_sprint/settings.py
+++ fast_design_sprint/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '9nxl-95wsdi6i+1drrksz=h!8@#o3rjj&&j_v1mab*a&!%g19i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
